f71c60ef5b1791c3239e810f1b3f10e7cf333c6b
Pierre Charpentier <charpentier.pierre@outlook.com>
2017-09-04 16:54:06 +0200

    Semicolons and app name

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
-const express = require('express')
-const bodyParser = require('body-parser')
+const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
-const mongoose = require('mongoose')
-const config = require('./config.json')
-//const grades = require('./routes/api/grades/grades')
-//const sections = require('./routes/api/grades/sections')
-const calendar = require('./routes/agenda/calendar')
+const mongoose = require('mongoose');
+const config = require('./config.json');
+//const grades = require('./routes/api/grades/grades');
+//const sections = require('./routes/api/grades/sections');
+const calendar = require('./routes/agenda/calendar');
 
-mongoose.connect(config.database.url)
+mongoose.connect(config.database.url);
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.use("/calendar", calendar);
 
 app.listen(3000, () => {
-    console.log('Server is listening!')
-})
+    console.log('Server is listening!');
+});
