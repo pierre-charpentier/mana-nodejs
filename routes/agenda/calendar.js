85b1f040b9475343bf824ae9f89a636b1ae3fbb7
Pierre Charpentier <charpentier.pierre@outlook.com>
2017-09-04 16:54:46 +0200

    Work on calendar API

--- routes/agenda/calendar.js
+++ routes/agenda/calendar.js
@@ -1,11 +1,63 @@
 const router = require('express').Router();
 const ical = require('ical');
+const Moment = require('moment');
 
-const icalUrl = "https://planning.univ-rennes1.fr/jsp/custom/modules/plannings/anonymous_cal.jsp?resources=1879&projectId=3&calType=ical&firstDate=2016-09-01&lastDate=2017-09-30";
+function buildIcalURL(opts) {
+    let url = "";
+    let { resources, projectId, firstDate, lastDate } = { ...opts };
+    const baseURL = "https://planning.univ-rennes1.fr/jsp/custom/modules/plannings/anonymous_cal.jsp?calType=ical";
 
-router.get('/', (req, res) => {
-    ical.fromURL(icalUrl, {}, (err, data) => {
+    if (resources && projectId) { // Might need to better validate that
+        firstDate = firstDate ? firstDate : Moment().subtract(10, 'days').format("YYYY-MM-DD");
+        lastDate = lastDate ? lastDate : Moment().add(30, 'days').format("YYYY-MM-DD");
+
+        url += baseURL;
+        url += "&resources=" + resources;
+        url += "&projectId=" + projectId;
+        url += "&firstDate=" + firstDate;
+        url += "&lastDate=" + lastDate;
+    }
+
+    return url;
+}
+
+router.get('/:resources', (req, res) => {
+    const { resources } = { ...req.params };
+    const { projectId, firstDate, lastDate } = { ...req.query };
+    
+    let url = buildIcalURL({ resources: resources, projectId: projectId, firstDate: firstDate, lastDate: lastDate });
+
+    ical.fromURL(url, {}, (err, data) => {
         res.json(data);
+    });
+});
+
+router.get('/:resources/byDay', (req, res) => {
+    const { resources } = { ...req.params };
+    const { projectId, firstDate, lastDate } = { ...req.query };
+    
+    let url = buildIcalURL({ resources: resources, projectId: projectId, firstDate: firstDate, lastDate: lastDate });
+
+    ical.fromURL(url, {}, (err, data) => {
+        let sortedData = {};
+
+        for (let event in data) {
+            let startDay = Moment(data[event].start).format('YYYY-MM-DD');
+            
+            if (!sortedData.hasOwnProperty(startDay)) {
+                sortedData[startDay] = [];
+            }
+
+            sortedData[startDay].push(data[event]);
+        }
+
+        for (let day in sortedData) {
+            sortedData[day].sort((a, b) => {
+                return Moment(a.start).isAfter(b.start);
+            });
+        }
+
+        res.json(sortedData);
     });
 });
 
