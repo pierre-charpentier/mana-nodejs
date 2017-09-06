5247ac648cf648f9e808cc9605df1ae75a624e95
Pierre Charpentier <charpentier.pierre@outlook.com>
2017-09-06 13:15:54 +0200

    Extract teachers names from event description

--- routes/agenda/calendar.js
+++ routes/agenda/calendar.js
@@ -5,13 +5,14 @@ const Moment = require('moment');
 function buildIcalURL(opts) {
     let url = "";
     let { resources, projectId, firstDate, lastDate } = { ...opts };
-    const baseURL = "https://planning.univ-rennes1.fr/jsp/custom/modules/plannings/anonymous_cal.jsp?calType=ical";
+    const baseURL = process.env.ICAL_BASE_URL;
 
     if (resources && projectId) { // Might need to better validate that
-        firstDate = firstDate ? firstDate : Moment().subtract(10, 'days').format("YYYY-MM-DD");
+        firstDate = firstDate ? firstDate : Moment().format("YYYY-MM-DD");
         lastDate = lastDate ? lastDate : Moment().add(30, 'days').format("YYYY-MM-DD");
 
         url += baseURL;
+        url += "?calType=ical";
         url += "&resources=" + resources;
         url += "&projectId=" + projectId;
         url += "&firstDate=" + firstDate;
@@ -21,44 +22,53 @@ function buildIcalURL(opts) {
     return url;
 }
 
-router.get('/:resources', (req, res) => {
+function getResources(byDay, req, res) {
     const { resources } = { ...req.params };
     const { projectId, firstDate, lastDate } = { ...req.query };
+
     
     let url = buildIcalURL({ resources: resources, projectId: projectId, firstDate: firstDate, lastDate: lastDate });
 
+    console.log(url);
     ical.fromURL(url, {}, (err, data) => {
-        res.json(data);
+        // This loop is done twice, could find a way to merge them
+        // Extract teacher name from summary data
+        for (let event in data) {                
+            data[event].teachers = data[event].description.match(/[A-Z]*\s([A-Z]{1}\.)+/g);
+        }
+
+        if (byDay) {
+            let sortedData = {};
+
+            for (let event in data) {
+                let startDay = Moment(data[event].start).format('YYYY-MM-DD');
+                
+                if (!sortedData.hasOwnProperty(startDay)) {
+                    sortedData[startDay] = [];
+                }
+
+                sortedData[startDay].push(data[event]);
+            }
+
+            for (let day in sortedData) {
+                sortedData[day].sort((a, b) => {
+                    return Moment(a.start).isAfter(b.start);
+                });
+            }
+
+            res.json(sortedData);
+        } else {
+            res.json(data);
+        }
     });
+}
+
+router.get('/:resources', (req, res) => {
+    getResources(false, req, res);
 });
 
 router.get('/:resources/byDay', (req, res) => {
-    const { resources } = { ...req.params };
-    const { projectId, firstDate, lastDate } = { ...req.query };
-    
-    let url = buildIcalURL({ resources: resources, projectId: projectId, firstDate: firstDate, lastDate: lastDate });
-
-    ical.fromURL(url, {}, (err, data) => {
-        let sortedData = {};
-
-        for (let event in data) {
-            let startDay = Moment(data[event].start).format('YYYY-MM-DD');
-            
-            if (!sortedData.hasOwnProperty(startDay)) {
-                sortedData[startDay] = [];
-            }
-
-            sortedData[startDay].push(data[event]);
-        }
-
-        for (let day in sortedData) {
-            sortedData[day].sort((a, b) => {
-                return Moment(a.start).isAfter(b.start);
-            });
-        }
-
-        res.json(sortedData);
-    });
+    getResources(true, req, res);
 });
 
 module.exports = router;
